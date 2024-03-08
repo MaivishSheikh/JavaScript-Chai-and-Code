8960523930e28b04d001360a3f8ce45ad8c8c8ed
Maivish Sheikh <144207611+MaivishSheikh@users.noreply.github.com>
2024-03-08 13:49:38 +0000

    control statement js

---
+++ control_statements.js
@@ -0,0 +1,13 @@
+if(2 == "2") {
+    console.log("True")
+}
+// == only checks values
+
+if(2 === "2") {
+    console.log("Equals")
+}
+// whereas === also checks the datatypes along with values
+
+// shorthand property
+if(2 == "2") console.log("test1"), console.log("test2");
+// not recommended, bad practice
