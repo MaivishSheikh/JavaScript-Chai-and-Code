98239b0c7edb9dee5964243f154c21893da14271
Maivish Sheikh <144207611+MaivishSheikh@users.noreply.github.com>
2024-03-09 10:22:47 +0000

    JavaScript Tutorial

---
+++ JavaScript/two_iteration.js
@@ -0,0 +1,19 @@
+// let i = 0
+
+// while(i <= 10) {
+//     console.log(i)
+//     i = i + 2
+// }
+
+let myArr = ["Keqing", "Venti", "Amber", "Gaming", "Barbara", "Xinqiu"]
+let i = 0
+
+// while(i < myArr.length) {
+//     console.log(myArr[i])
+//     i++
+// }
+
+do {
+    console.log(i)
+    i++
+} while(i < 8)
